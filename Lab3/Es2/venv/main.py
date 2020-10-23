051da9296f051a10bd8e6c129947e77c4a0a832d
GabrieleGioetto <gabriele.gioetto@gmail.com>
2020-10-23 21:02:37 +0200

    Es 3.2 added external json

--- Lab3/Es2/venv/main.py
+++ Lab3/Es2/venv/main.py
@@ -1,24 +1,25 @@
 import itertools as it
+import json
+
+
+# from mlxtend.frequent_patterns import fpgrowth
+# from mlxtend.frequent_patterns import association_rules
 
 
 def listContainTuple(tuple, list):
-    return all(letter in list for letter in tuple)
+    return all(element in list for element in tuple)
 
 
-def firstLettersEqual(tuple1, tuple2):
+def firstElementsEqual(tuple1, tuple2):
     length = len(tuple1)
     for i in range(length - 1):
         if tuple1[i] != tuple2[i]:
             return False
-
     return True
 
 
-# Test
-# print(firstLettersEqual('a', 'b'))
-# print(firstLettersEqual(('a', 'b'), ('a', 'c')))
-# print(firstLettersEqual(('a', 'b', 'c'), ('a', 'b', 'd')))
-# print(firstLettersEqual(('a', 'c', 'f'), ('a', 'b', 'd')))
+# -------------------------------------------------------------- #
+# 1° parte
 
 data = [
     ['a', 'b'],
@@ -37,7 +38,7 @@ C1 = set([(item,) for itemList in data for item in itemList])
 C1 = sorted(C1)
 
 C = [C1]  # lista di liste di tuple
-L = []
+L = []  # Lista di dizionari con valori: ((tupla elementi), frequenza)
 i = 0
 minsup = 2
 
@@ -59,7 +60,7 @@ while len(C[i]) > 0:
         for k in range(j + 1, tupleCount):
             C.append([])
             listTuples = list(L[i].keys())
-            if firstLettersEqual(listTuples[j], listTuples[k]):
+            if firstElementsEqual(listTuples[j], listTuples[k]):
                 C[i + 1].append(listTuples[j] + (listTuples[k][-1],))  # Aggiungo l'ultima lettera
 
     for j in range(len(C[i + 1]) - 1, -1, -1):  # Ciclo al contrario per evitare errori di index nel del
@@ -71,4 +72,54 @@ while len(C[i]) > 0:
     i += 1
 
 print(L)
-print(sum([len(k) for k in L]))
+print(f"Number of elements in L: {sum([len(k) for k in L])}")
+print("---------------------------------------------------------------------------------------------")
+
+# --------------------------------------------------------------------------------------------- #
+# 2° parte
+
+with open("modified_coco.json") as f:
+    data = json.load(f)
+
+    dataAnnotations = [d["annotations"] for d in data]
+    C1 = set([(item,) for itemList in dataAnnotations for item in itemList])
+    C1 = sorted(C1)
+
+    C = [C1]  # lista di liste di tuple
+    L = []
+    i = 0
+    minsup = 0.02
+    T = len(dataAnnotations)
+
+    while len(C[i]) > 0:
+
+        # CALCOLO Li
+        L.append({})
+        for letterTuple in C[i]:
+            L[i][letterTuple] = 0
+            for itemList in dataAnnotations:
+                if listContainTuple(letterTuple, itemList):
+                    L[i][letterTuple] += 1
+            L[i][letterTuple] /= T  # Calculate support
+            if L[i][letterTuple] < minsup:  # PRUNING min support
+                del L[i][letterTuple]
+
+        # CALCOLO Ci+1
+        tupleCount = len(L[i])
+        for j in range(tupleCount):
+            for k in range(j + 1, tupleCount):
+                C.append([])
+                listTuples = list(L[i].keys())
+                if firstElementsEqual(listTuples[j], listTuples[k]):
+                    C[i + 1].append(listTuples[j] + (listTuples[k][-1],))  # Aggiungo l'ultima lettera
+
+        for j in range(len(C[i + 1]) - 1, -1, -1):  # Ciclo al contrario per evitare errori di index nel del
+            if not all(
+                    letterTuple in list(L[i].keys()) for letterTuple in
+                    list(it.combinations(C[i + 1][j], len(C[i + 1][j]) - 1))):
+                del C[i + 1][j]  # PRUNING
+
+        i += 1
+
+    print(L)
+    print("---------------------------------------------------------------------------------------------")
