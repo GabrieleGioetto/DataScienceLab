550c09035a2534522543dccb3a4ef7e2f464a43a
GabrieleGioetto <gabriele.gioetto@gmail.com>
2020-11-05 16:35:33 +0100

    Finished es1 lab5

--- Lab5/Es1/main.py
+++ Lab5/Es1/main.py
@@ -1,6 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+def getIdCellByLonLat(lon, lat, minLon, minLat, number_of_grids):
+    latGrid = (lat - minLat) / (maxLat - minLat) * number_of_grids
+    lonGrid = (lon - minLon) / (maxLon - minLon) * number_of_grids
+
+    idCell = lonGrid + latGrid * number_of_grids
+    return idCell
+
+
 with open("pois_all_info") as f_all_info:
     with open("ny_municipality_pois_id.csv") as f_ids:
         data = pd.read_csv(f_all_info, sep='\t')
@@ -30,14 +39,20 @@ with open("pois_all_info") as f_all_info:
         topPublicTransportData = data[data["public_transport"].isin(topData["public_transport"].index)].copy()
         topHighwayData = data[data["highway"].isin(topData["highway"].index)].copy()
 
+        topData = pd.concat([topAmenityData, topShopData, topPublicTransportData, topHighwayData])
+
         plt.scatter(x=topAmenityData["@lon"], y=topAmenityData["@lat"], marker=".", zorder=1, color="blue")
         plt.scatter(x=topShopData["@lon"], y=topShopData["@lat"], marker=".", zorder=1, color="red")
-        plt.scatter(x=topPublicTransportData["@lon"], y=topPublicTransportData["@lat"], marker=".", zorder=1, color="black")
+        plt.scatter(x=topPublicTransportData["@lon"], y=topPublicTransportData["@lat"], marker=".", zorder=1,
+                    color="black")
         plt.scatter(x=topHighwayData["@lon"], y=topHighwayData["@lat"], marker=".", zorder=1, color="pink")
 
         axes = plt.gca()
         x_lim_scatter = axes.get_xlim()
         y_lim_scatter = axes.get_ylim()
+
+        minLon, maxLon = x_lim_scatter
+        minLat, maxLat = y_lim_scatter
 
         ext = [x_lim_scatter[0], x_lim_scatter[1], y_lim_scatter[0], y_lim_scatter[1]]
 
@@ -47,3 +62,10 @@ with open("pois_all_info") as f_all_info:
 
         plt.gca().set_aspect(aspect)
         plt.show()
+
+        number_of_grids = 15  # grid 15 X 15
+        topData["gridId"] = getIdCellByLonLat(topData["@lon"], topData["@lat"], minLon, minLat, number_of_grids)
+        topData["gridId"] = topData["gridId"].astype(int)
+
+        groupedDataByCell = topData.groupby(["gridId"]).count().drop(["@lat", "@lon", "@type"], axis=1)
+        print(groupedDataByCell)
