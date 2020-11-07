7be8c3dba7248f0f022e59e74d1ccee090d65201
GabrieleGioetto <gabriele.gioetto@gmail.com>
2020-11-07 17:53:45 +0100

    Added es5.2

---
+++ Lab5/Es2/main.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+with open("831394006_T_ONTIME.csv") as f:
+    data = pd.read_csv(f, parse_dates=["FL_DATE"])
+
+    # print(data)
+    # print("----------------------------------------------------")
+    # print(data.info())
+    # print("----------------------------------------------------")
+    # print(data.describe())
+    # print("----------------------------------------------------")
+    # print(data["FL_DATE"].min())
+
+    # 3
+    data = data[data["CANCELLED"] == 0]  # Filtro voli non cancellati
+
+    # 4
+    print(data.groupby("UNIQUE_CARRIER").count())
+    print(data.groupby("UNIQUE_CARRIER").mean()["ARR_DELAY"])
+
+    # 5
+    data["weekday"] = data["FL_DATE"].dt.dayofweek
+    data["delaydelta"] = data["ARR_DELAY"] - data["DEP_DELAY"]
+
+    # 6
+    print(data.groupby("weekday").mean()["ARR_DELAY"])
+    data.groupby("weekday").mean()["ARR_DELAY"].plot(kind="bar")
+    plt.show()
+
+    # 7
+    avg_arr_delay_by_carrier_on_weekend = data[data["weekday"] >= 5].groupby(["UNIQUE_CARRIER", "weekday"]).mean()[
+        "ARR_DELAY"]
+    avg_arr_delay_by_carrier_on_weekday = data[data["weekday"] < 5].groupby(["UNIQUE_CARRIER", "weekday"]).mean()[
+        "ARR_DELAY"]
+    print(avg_arr_delay_by_carrier_on_weekend)
+    print(avg_arr_delay_by_carrier_on_weekday)
+
+    # 8
+    # indexes = [data["UNIQUE_CARRIER"], data["ORIGIN"], data["DEST"], data["FL_DATE"]]
+    # dataIndexed = pd.DataFrame(data, index=[indexes])
+    dataGrouped = data.set_index(["UNIQUE_CARRIER", "ORIGIN", "DEST", "FL_DATE"])
+    print(data.index)
+    print("--------------------------------------------")
+
+    # 9
+    print(dataGrouped.loc[["AA", "DL"], "LAX", :, :][["DEP_TIME", "DEP_DELAY"]])
+    print("--------------------------------------------")
+
+    # 10
+    dataFirstWeek = dataGrouped[pd.Int64Index(dataGrouped.index.get_level_values("FL_DATE").isocalendar().week) == 1]
+    print(f'MEDIA:  {dataFirstWeek.loc[:, :, "LAX", :]["ARR_DELAY"].mean()}')
